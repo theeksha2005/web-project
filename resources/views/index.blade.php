4cd2cfff1a9cd40590506e57d4538ea90fa4ed4a
theeksha2005 <gaveshaniimodya@gmail.com>
2025-06-29 12:12:05 +0530

    Initial commit: Laravel + Tailwind + Vite setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div class="bg-pink-500 text-white p-4 rounded-lg">
+  Tailwind CSS is working!
+</div>
+  </body>
+</html>
